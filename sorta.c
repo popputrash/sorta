207f3648c37000ab569d9269ab39f0d8673a2c57
popputrash <poppu@syrobonkus.localdomain>
2022-10-06 14:18:26 +0000

    added visualizer for bubblesort

--- sorta.c
+++ sorta.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <unistd.h>
 #include <stdlib.h>
-int data[20000];
+int data[20];
 
 int bubbleSort(int array[], int size){
     for (int i = 0; i < size - 1; i++){
@@ -9,10 +11,25 @@ int bubbleSort(int array[], int size){
                 int tmp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = tmp;
+                visualizeArray(array, size);
             }
         }
     }  
 }
+
+
+void visualizeArray(int array[], int size){ 
+  printf("\x1b[2J");
+  for(int i = 0; i < size; i++){
+    printf("- ");
+    for(int j = 0; j < array[i]; j++){
+      printf("O");
+    }
+    printf("\n");;
+  }
+  usleep(70000);
+}
+
 
 void printArray(int array[], int size){
     for (int i = 0; i < size; i++){
@@ -23,18 +40,15 @@ void printArray(int array[], int size){
 int main(){
 
     int size = sizeof(data)/sizeof(data[0]);
-
-    for (int i = 0; i < size; i++){
-        data[i] = rand() % 20000;
-    }
     
-    printArray(data, size);
+    for (int i = 0; i < size; i++){
+        data[i] = rand() % 10;
+    }
 
     bubbleSort(data, sizeof(data)/sizeof(data[0]));
-    
+
     printf("\n");
 
-    printArray(data, size);
 
 }
 
